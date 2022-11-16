23c16ac214457f37eb282755cba9151811dfab7e
Tayo Adekoya <Tayoadekoya96@gmail.com>
2022-11-15 20:57:15 -0700

    update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-# Creates a resource group if one doesn't exist
+# Create a resource group if one doesn't exist
 resource "azurerm_resource_group" "RG" {
   name     = var.resource_group_name
   location = var.resource_group_location
